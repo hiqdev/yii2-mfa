6355ff7beeee4032d91e6bc63defe5c539f60b59
Andrii Vasyliev <sol@hiqdev.com>
2017-10-03 22:13:54 +0300

    inited tests

---
+++ tests/_bootstrap.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Multi-factor authentication for Yii2 projects
+ *
+ * @link      https://github.com/hiqdev/yii2-mfa
+ * @package   yii2-mfa
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2016-2017, HiQDev (http://hiqdev.com/)
+ */
+
+error_reporting(E_ALL & ~E_NOTICE);
+
+$bootstrap = __DIR__ . '/../src/_bootstrap.php';
+
+require_once file_exists($bootstrap) ? $bootstrap : __DIR__ . '/../vendor/autoload.php';
+
+/*
+ * Ensures compatibility with PHPUnit 6.x
+ */
+if (!class_exists('PHPUnit_Framework_Constraint') && class_exists('PHPUnit\Framework\Constraint\Constraint')) {
+    abstract class PHPUnit_Framework_Constraint extends \PHPUnit\Framework\Constraint\Constraint
+    {
+    }
+}
+if (!class_exists('PHPUnit_Framework_TestCase') && class_exists('PHPUnit\Framework\TestCase')) {
+    abstract class PHPUnit_Framework_TestCase extends \PHPUnit\Framework\TestCase
+    {
+    }
+}
